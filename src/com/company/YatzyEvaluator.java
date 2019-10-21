e975c59e598573f09fd575c5389b44946a013e26
Fredrik Jönsson <Fredrikjay@gmail.com>
2019-10-21 12:32:45 +0200

    Junit test

--- src/com/company/YatzyEvaluator.java
+++ src/com/company/YatzyEvaluator.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class YatzyEvaluator {
+
+    public static boolean isYatzy(int[] roll) {
+        return true;
+    }
+    public static boolean isFourOfAKind(int[] roll){
+        return true;
+    }
+    public static boolean isThreeofAKind(int[] roll){
+        return true;
+    }
 }
---
+++ src/com/company/YatzyEvaluatorTest.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import static org.junit.Assert.*;
+
+public class YatzyEvaluatorTest {
+
+    @org.junit.Test
+    public void isYatzy() {
+    }
+
+    @org.junit.Test
+    public void isFourOfAKind() {
+    }
+
+    @org.junit.Test
+    public void isThreeofAKind() {
+    }
+}
