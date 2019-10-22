81c8310646ac4803fa61217187fb85526249a535
Fredrik Jönsson <Fredrikjay@gmail.com>
2019-10-22 20:34:26 +0200

    update3

--- src/com/company/YatzyEvaluator.java
+++ src/com/company/YatzyEvaluator.java
@@ -3,23 +3,24 @@ package com.company;
 public class YatzyEvaluator {
 
     public static boolean isYatzy(int[] roll) {
-        for (int i = 0;i<roll.length;i++) {
-            for (int j = 1; j < roll.length;j++){
-                if(roll[i] != roll[j]){
+        for (int i = 0; i < roll.length; i++) {
+            for (int j = 1; j < roll.length; j++) {
+                if (roll[i] != roll[j]) {
                     return false;
                 }
             }
         }
         return true;
     }
-    public static boolean isFourOfAKind(int[] roll){
-        int countMatches=0;
-        int index=0;
-        for (int j = 0;j<roll.length;j++) {
-            if(countMatches==4){
+
+    public static boolean isFourOfAKind(int[] roll) {
+        int countMatches = 0;
+        int index = 0;
+        for (int j = 0; j < roll.length; j++) {
+            if (countMatches == 4) {
                 return true;
             }
-            countMatches=0;
+            countMatches = 0;
             index++;
             for (int i = 0; i < roll.length; i++) {
                 if (index == roll[i]) {
@@ -30,20 +31,61 @@ public class YatzyEvaluator {
         return false;
 
     }
-    public static boolean isThreeofAKind(int[] roll){
-        int countMatches=0;
-        int index=0;
-        for (int j = 0;j<roll.length;j++) {
-            if(countMatches==3){
+
+    public static boolean isThreeofAKind(int[] roll) {
+        int countMatches = 0;
+        int index = 0;
+        for (int j = 0; j < roll.length; j++) {
+            if (countMatches == 3) {
                 return true;
             }
-            countMatches=0;
+            countMatches = 0;
             index++;
             for (int i = 0; i < roll.length; i++) {
                 if (index == roll[i]) {
                     countMatches++;
                 }
             }
+        }
+        return false;
+    }
+
+    public static boolean isBigStraight(int[] roll) {
+        int countMatches = 0;
+        int index = 0;
+        for (int j = 0; j < roll.length; j++) {
+            index=2;
+
+            for (int i = 0; i < roll.length; i++) {
+                if (index == roll[i]) {
+                    countMatches++;
+                }
+                index++;
+            }
+        }
+        if (countMatches == 5) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isSmallStraight(int[] roll){
+        int countMatches = 0;
+        int index = 0;
+        for (int j = 0; j < roll.length; j++) {
+            index=1;
+
+            for (int i = 0; i < roll.length; i++) {
+                if (index == roll[i]) {
+                    countMatches++;
+                    break;
+                }
+                index++;
+
+            }
+        }
+        if (countMatches == 5) {
+            return true;
         }
         return false;
     }
--- src/com/company/YatzyEvaluatorTest.java
+++ src/com/company/YatzyEvaluatorTest.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class YatzyEvaluatorTest {
@@ -38,5 +40,27 @@ public class YatzyEvaluatorTest {
         int [] roll2 = {3,1,1,3,3};
         isThreeofAKind=YatzyEvaluator.isThreeofAKind(roll2);
         assertTrue(isThreeofAKind);
+    }
+
+    @Test
+    public void isBigStraight() {
+        int [] roll = {2,2,4,5,3};
+        boolean isBigStraight=YatzyEvaluator.isBigStraight(roll);
+        assertFalse(isBigStraight);
+
+        int [] roll2 = {2,6,3,4,5};
+        isBigStraight=YatzyEvaluator.isBigStraight(roll2);
+        assertTrue(isBigStraight);
+    }
+
+    @Test
+    public void isSmallStraight() {
+        int [] roll = {2,2,4,5,3};
+        boolean isSmallStraight=YatzyEvaluator.isSmallStraight(roll);
+        assertFalse(isSmallStraight);
+
+        int [] roll2 = {1,2,3,4,5};
+        isSmallStraight=YatzyEvaluator.isSmallStraight(roll2);
+        assertTrue(isSmallStraight);
     }
 }
